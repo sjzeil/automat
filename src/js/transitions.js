0e4088d844328b02419e619350cd3f7d7ad2a566
Steven Zeil <zeil@cs.odu.edu>
2021-05-22 09:27:38 -0400

    transitions can be drawn

--- src/js/transitions.js
+++ src/js/transitions.js
@@ -12,7 +12,7 @@ var Transition = fabric.util.createClass(fabric.Text, {
 		this.set('from', fromState);
 		this.set('to', toState);
 		this.set('label', options.label || '');
-		this.set('curved', options.initial || false);
+		this.set('curved', options.curved || false);
 
         this.callSuper('initialize', this.label, 
             {fontSize: 14, fontFamily: "cursive"});
@@ -23,21 +23,7 @@ var Transition = fabric.util.createClass(fabric.Text, {
         let y1 = toState.top + toState.circle.radius;
         let dx = x1 - x0;
         let dy = y1 - y0;
-        let angle = Math.atan2(dy, dx);
-        console.log ('dx ' + dx + '  angle ' + angle);
-        
-
-        if (Math.abs(dx) <= this.width) {
-            console.log("** override x pos")
-            this.left = (x0 + x1) / 2 + Transition.minTextOffset;
-        } else {
-            this.left = (x0 + x1) / 2 - this.width / 2;
-        }
-        if (angle >= 0) {
-            this.top = (y0 + y1) / 2;
-        } else {
-            this.top = (y0 + y1) / 2 - this.height;
-        }
+        let angle = Math.atan2(dy, dx);        
 
 		
 	},
@@ -52,43 +38,90 @@ var Transition = fabric.util.createClass(fabric.Text, {
         let dy = y1 - y0;
         let angle = Math.atan2(dy, dx);
 
+        let dist = Math.sqrt(dx*dx + dy*dy);
+        let lineLen = dist - this.from.circle.radius - this.to.circle.radius;
 
-        let lineLen = Math.sqrt(dx*dx + dy*dy) - this.from.circle.radius - this.to.circle.radius;
+        let xc = (x0 + x1) / 2;
+        let yc = (y0 + y1) / 2;
 
-        if (Math.abs(dx) <= this.width) {
-            console.log("** override x pos")
-            this.left = (x0 + x1) / 2 + Transition.minTextOffset;
-        } else {
-            this.left = (x0 + x1) / 2 - this.width / 2;
-        }
-        if (angle < 0) {
-            this.top = (y0 + y1) / 2;
-        } else {
-            this.top = (y0 + y1) / 2 - this.height;
-        }
-        this.callSuper('render', ctx);
+        let direction = (this.from.label < this.to.label) ? 1 : -1;
+        let exitAngle = angle - ((this.curved) ? Transition.angleOffset : 0);
+        let fromXc = this.from.left + this.from.circle.radius;
+        let fromYc = this.from.top + this.from.circle.radius;
+        let exitX = fromXc + this.from.circle.radius*Math.cos(exitAngle);
+        let exitY = fromYc + this.from.circle.radius*Math.sin(exitAngle);
+
+
+        let entryAngle = angle + Math.PI + ((this.curved) ? Transition.angleOffset : 0);
+        let toXc = this.to.left + this.to.circle.radius;
+        let toYc = this.to.top + this.to.circle.radius;
+        let entryX = toXc + this.to.circle.radius*Math.cos(entryAngle);
+        let entryY = toYc + this.to.circle.radius*Math.sin(entryAngle);
+
+        let arcOffset = ((this.curved) ? Transition.curveOffset : 0);
+        let textOffset = ((this.curved) ? 0.75*Transition.curveOffset : Transition.minTextOffset);
+
+        let offSetAngle = angle - Math.PI / 2.0;
+        let arcOffsetX = xc + arcOffset * Math.cos(offSetAngle);
+        let arcOffsetY = yc + arcOffset * Math.sin(offSetAngle);
+
+        let textX = xc + textOffset * Math.cos(offSetAngle);
+        let textY = yc + textOffset * Math.sin(offSetAngle);
 
         ctx.save();
-        ctx.translate(x0, y0);
-        ctx.rotate(angle);
-        ctx.translate(this.from.circle.radius, 0);
         ctx.beginPath();
-        ctx.moveTo(0,0);
-        ctx.lineTo(lineLen, 0);
+        ctx.moveTo(exitX, exitY);
+        ctx.quadraticCurveTo(arcOffsetX, arcOffsetY, entryX, entryY);
         ctx.strokeStyle='black';
-		ctx.stroke();
+        ctx.stroke();
+
+        // draw the arrowhead
+        ctx.save();
+        ctx.translate(entryX, entryY);
+        ctx.rotate(entryAngle + Math.PI);
         ctx.beginPath();
-        ctx.moveTo(lineLen, 0);
-        ctx.lineTo(lineLen-Transition.arrowSize, Transition.arrowSize/2);
-        ctx.lineTo(lineLen-Transition.arrowSize, -Transition.arrowSize/2);
-        ctx.lineTo(lineLen, 0);
+        ctx.moveTo(0, 0);
+        ctx.lineTo(-Transition.arrowSize, Transition.arrowSize/2);
+        ctx.lineTo(-Transition.arrowSize, -Transition.arrowSize/2);
+        ctx.lineTo(0, 0);
         ctx.fillStyle = 'black';
         ctx.fill();
         ctx.stroke();
         ctx.restore();
-	},
+        ctx.restore();
+
+        
+        if (angle < -Math.PI / 2) {
+            this.left = textX - this.width;
+            this.top = textY;
+        } else if (angle < 0) {
+            this.left = textX - this.width;
+            this.top = textY - this .height
+        } else if (angle < Math.PI / 2) {
+            this.left = textX;
+            this.top = textY - this.height;
+        } else {
+            this.left = textX;
+            this.top = textY;
+        }
+        this.set ('selectable',  true);
+        this.callSuper('render', ctx);
+},
 
 });
 
+function drawPoint(ctx,x,y,color)
+{
+    ctx.save();
+    ctx.beginPath();
+    ctx.fillStyle = color;
+    ctx.fillRect(x-1, y-1, 3, 3);
+    ctx.restore();
+}
+
+
+
 Transition.arrowSize = 10;
 Transition.minTextOffset = 5;
+Transition.curveOffset = 30;
+Transition.angleOffset = 2.0 * Math.PI * (15.0 / 360.0);
--- src/js/transitionsDemo.js
+++ src/js/transitionsDemo.js
@@ -2,18 +2,23 @@ var canvas = new fabric.Canvas('transitionsDemo');
 
 var state1 = new State({label: "0", left: 100, top: 100, initial: true});
 
-var state2 = new State({label: "9999", left: 200, top: 200, selected: true, final: true});
+var state2 = new State({label: "9999", left: 200, top: 300, final: true});
 
-var state3 = new State({label: "abc", left: 250, top: 100, annotation: "Look\nhere!"});
+var state3 = new State({label: "abc", left: 250, top: 100, selected: true, annotation: "Look\nhere!"});
 
-var transition13 = new Transition(state1, state3, {label: "text", curved: 'false'} );
-var transition21 = new Transition(state2, state1, {label: "a\nb\nc", curved: 'false'} );
+var transition13 = new Transition(state1, state3, {label: "text", curved: false} );
+var transition21 = new Transition(state2, state1, {label: "a\nb\nc", curved: false} );
+var transition23 = new Transition(state2, state3, {label: "99->abc", curved: true} );
+var transition32 = new Transition(state3, state2, {label: "abc->99", curved: true} );
+
 
 canvas.add(state1);
 canvas.add(state2);
 canvas.add(state3);
 canvas.add(transition13);
 canvas.add(transition21);
+canvas.add(transition23);
+canvas.add(transition32);
 
 
 canvas.renderAll();
