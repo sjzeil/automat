3b7d51d552279bce062708fc09d5dd299e8e7e0e
Steven Zeil <zeil@cs.odu.edu>
2021-09-27 10:54:21 -0400

    RegExps are printed on canvas

--- src/shared/js/regularExpression.ts
+++ src/shared/js/regularExpression.ts
@@ -1,5 +1,6 @@
 import { JavascriptModulesPlugin } from 'webpack';
 import { FormalLanguage } from './formalLanguage';
+import { fabric } from 'fabric';
 
 
 
@@ -10,16 +11,22 @@ import { FormalLanguage } from './formalLanguage';
  */
 
 export class RegularExpression extends FormalLanguage {
-    constructor(canvas: fabric.Canvas) {
-        super(canvas);
+    constructor(canvas: fabric.Canvas, user: string) {
+        super(canvas, user);
         this.regexp = "";
         this.specification = "regexp";
+        this.rendering = new fabric.Text("", {left: 10, top: 10, fontSize:16, fontWeight: 'bold'});
+        canvas.add(this.rendering);
     }
 
     regexp: string;
+    rendering: fabric.Text;
     
     render() {
         let result = this.regexp.replace(/@/g, '\u03B5');
+        this.rendering.set('text', result);
+        this._canvas.clear();
+        this._canvas.add(this.rendering);
         return result;
     }
 
