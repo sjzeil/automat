cd6e8f092851c5680637f04f5174247011c5299f
Steven Zeil <zeil@cs.odu.edu>
2021-12-04 11:04:26 -0500

    New Lang editor: buttons arrangedin a column

--- automatWeb/src/js/newLanguageEditor.tsx
+++ automatWeb/src/js/newLanguageEditor.tsx
@@ -31,9 +31,9 @@ export
             <div className="editors">
                 Create a new...
                 <div className="centered">
-                    <input type="button" value="Finite Automaton" onClick={mainEditor.newFA} />
+                    <input type="button" value="Finite Automaton" onClick={mainEditor.newFA} /><br />
                     <input type="button" value="Regular Expression" onClick={mainEditor.newRE} /><br />
-                    <input type="button" value="Context-Free Grammar" onClick={mainEditor.newCFG} />
+                    <input type="button" value="Context-Free Grammar" onClick={mainEditor.newCFG} /><br />
                     <input type="button" value="Push-Down Automaton" onClick={mainEditor.newPDA} /><br />
                     <input type="button" value="Turing Machine" onClick={mainEditor.newTM} />
                 </div>
